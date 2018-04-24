41d1cf8031b0c515a2744bb144a443437d138137
LeoStephaniakwe <leonardomoreira050@gmail.com>
2018-04-24 11:01:06 -0300

    Desenvolvimento do exercício 01

---
+++ ExercicioWhile01.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+public class ExercicioWhile01{
+    public static void main(String [] args){
+        String nome = JOptionPane.showInputDialog(null, "Digite um nome:");
+        
+        /*while = (!nome.equalsIgnoreCase("Sair"))
+        */
+        while(!nome.equalsIgnoreCase("Sair"));
+            nome = JOptionPane.showInputDialog(null, "Digite o nome")
+
+        }
+}
+   
