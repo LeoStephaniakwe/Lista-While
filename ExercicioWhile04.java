e013814111ab197039e62953722913ac6115d901
Leonardo Moreira da Silva <leonardomoreira050@gmail.com>
2018-04-27 08:41:14 -0300

    merge

--- ExercicioWhile04.java
+++ ExercicioWhile04.java
@@ -10,7 +10,6 @@ public class ExercicioWhile04{
         int mediaAnoCarro = 0;
         double somaValor = 0;
         double mediaValor = 0;
-<<<<<<< HEAD
         int modeloA = 0, 
         modeloG =0;
          while(contador < carros){
@@ -34,28 +33,5 @@ public class ExercicioWhile04{
                                           "\nMédia total de valores: R$" + mediaValor +
                                           "\nQuantidade de carros que começam com a letra \"A\" = " + modeloA + 
                                           "\nQuantidade de carros que começam com a letra \"G\" = " + modeloG );
-=======
-
-
-        while(contador < carros){
-            String modelo = JOptionPane.showInputDialog("Qual o modelo");
-            int ano = Integer.parseInt(JOptionPane.showInputDialog("Ano?:"));
-            double valor = Double.parseDouble(JOptionPane.showInputDialog("Qual valor?"));
-
-            contador = contador + 1;
-            somaAno = somaAno + ano;
-            somaValor = somaValor + valor;
-
-            char letra = carro.charAt(0);
-            if(letra == 'g'){
-            }else if(letra == 'a');
-            }
-        }   
-            mediaAnoCarro = somaAno / carros;
-            mediaValor = somaValor / carros;
-        
-            JOptionPane.showMessageDialog(null, "Média de Ano Veicular: " + mediaAnoCarro + 
-                                          "\nMédia total de valores: R$" + mediaValor);
->>>>>>> ddae37a5082db2008f8a11bcbbf2781e15039239
     }
 }
